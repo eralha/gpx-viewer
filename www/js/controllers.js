9bf1f2bcc91c2ae74dfcec5ca9930fb2336f371b
Emanuel Ralha <ejr@netmais.local>
2017-01-26 18:37:10 +0000

    file system

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -173,31 +173,89 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope, $rootScope) {
+.controller('AccountCtrl', function($scope, $ionicPlatform, $fileFactory) {
   $scope.settings = $rootScope.settings;
 
-  function readDownloadFolder(){
-    alert(window.requestFileSystem);
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
-        function (fs) {
-            fs.root.getDirectory("/Download", {
-                create : false,
-                exclusive : false
-            }, function (dirEntry) { //success
-                //do here what you need to do with Download folder
-                alert(JSON.stringify(dirEntry));
-            }, function () {
-                //error getDirectory
-            });
-        },
-        function () {
-        //error requestFileSystem
-        }
-    );
-  }
+  var fs = new $fileFactory();
 
-  document.addEventListener("deviceready", function() {
-    readDownloadFolder();
-  }, false);//end device ready
+    $ionicPlatform.ready(function() {
+        fs.getEntriesAtRoot().then(function(result) {
+            $scope.files = result;
+        }, function(error) {
+            console.error(error);
+        });
+
+        $scope.getContents = function(path) {
+            fs.getEntries(path).then(function(result) {
+                $scope.files = result;
+                $scope.files.unshift({name: "[parent]"});
+                fs.getParentDirectory(path).then(function(result) {
+                    result.name = "[parent]";
+                    $scope.files[0] = result;
+                });
+            });
+        }
+    });
+
+
+})
+
+
+.factory("$fileFactory", function($q) {
+
+    var File = function() { };
+
+    File.prototype = {
+
+        getParentDirectory: function(path) {
+            var deferred = $q.defer();
+            window.resolveLocalFileSystemURI(path, function(fileSystem) {
+                fileSystem.getParent(function(result) {
+                    deferred.resolve(result);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntriesAtRoot: function() {
+            var deferred = $q.defer();
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+                var directoryReader = fileSystem.root.createReader();
+                directoryReader.readEntries(function(entries) {
+                    deferred.resolve(entries);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntries: function(path) {
+            var deferred = $q.defer();
+            window.resolveLocalFileSystemURI(path, function(fileSystem) {
+                var directoryReader = fileSystem.createReader();
+                directoryReader.readEntries(function(entries) {
+                    deferred.resolve(entries);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        }
+
+    };
+
+    return File;
 
 });
+
+
+;
