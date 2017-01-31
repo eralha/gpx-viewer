ef4807296aa10068f00f37261c2e3c1e2337915e
Emanuel Ralha <ejr@netmais.local>
2017-01-31 12:45:37 +0000

    add km to track length

--- www/js/services.js
+++ www/js/services.js
@@ -41,14 +41,37 @@ angular.module('starter.services', [])
       module.trk = {
         name : trkName,
         points : points,
-        wpts : wpts
+        wpts : wpts,
+        length : module.inKm(points)
       };
+
+      alert(module.trk.length);
 
     }
     catch(err) {
       alert(err.message);    
     }
     
+  }//end parse XML
+
+  module.kmTo = function(_a, a){ 
+      var e = Math, ra = e.PI/180; 
+      var b = _a.lat * ra, c = a.lat * ra, d = b - c; 
+      var g = _a.lng * ra - a.lng * ra; 
+      var f = 2 * e.asin(e.sqrt(e.pow(e.sin(d/2), 2) + e.cos(b) * e.cos 
+      (c) * e.pow(e.sin(g/2), 2))); 
+      return f * 6378.137; 
+  }
+
+  module.inKm = function(n){ 
+      var a = n, 
+          len = n.length, 
+          dist = 0; 
+
+      for (var i=0; i < len-1; i++) { 
+         dist += module.kmTo(a[i], a[i+1]); 
+      }
+      return dist; 
   }
 
 
